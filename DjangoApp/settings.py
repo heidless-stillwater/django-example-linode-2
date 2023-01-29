47d9def253084a1d2e312d3b1005854b49b3bc2c
heidless <rob.lockhart@yahoo.co.uk>
2023-01-29 16:42:43 +0000

    shortener install ALPHA

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -4,7 +4,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -17,8 +16,8 @@ DEBUG = True
 ALLOWED_HOSTS = [
     '212.71.248.219',
     '127.0.0.1',
+    'localhost',
 ]
-
 
 # Application definition
 
@@ -29,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'shortener'
 ]
 
 MIDDLEWARE = [
@@ -61,7 +61,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'DjangoApp.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -71,14 +70,6 @@ DATABASES = {
         'NAME': str(BASE_DIR / "db.sqlite3"),
     }
 }
-
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -98,7 +89,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -111,7 +101,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
--- DjangoApp/urls.py
+++ DjangoApp/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from shortener import views
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('shortener.urls')),
     path('other/', include('other.urls'))
 ]
